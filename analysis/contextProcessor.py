5cfd566a96163378a6c3163ae008d7be6ab59b3a
Eryk Dudkiewicz <1iekki44@gmail.com>
2025-12-08 08:38:20 +0100

    cleanup

--- analysis/contextProcessor.py
+++ analysis/contextProcessor.py
@@ -12,8 +12,16 @@ class ContextProcessor:
     transciberModel: Transcriber
     segments:list
     passage:list
+    pos_queries:list
+    neg_queries:list
+    topic_phrases:list
+    keywords:list
+    pos_embeds:torch.Tensor
+    neg_embeds:torch.Tensor
+    topic_vector:torch.Tensor
 
-    def __init__(self):
+
+    def __init__(self, params:dict):
         load_dotenv()
         SEMSEARCH_MODEL_PATH = os.getenv("SEMSEARCH_MODEL_PATH")
         TRANSCRIBER_MODEL_NAME = os.getenv("TRANSCRIBER_MODEL_NAME")
@@ -24,79 +32,46 @@ class ContextProcessor:
         self.segments = []
         self.passage = []
 
+        self.pos_queries = params['pos_queries']
+        self.neg_queries = params['neg_queries']
+        self.topic_phrases = params['topic_phrases']
+        self.keywords = params['keywords']
+
+        self.pos_embeds = self.semanticSearch.encode(self.pos_queries)
+        self.neg_embeds = self.semanticSearch.encode(self.neg_queries)
+        self.topic_vector = self.semanticSearch.topic_vec(self.topic_phrases)
+
+
     def process(self, audio_path:str):
-        self.transciberModel.stream_segment_to(audio_path, self.handle_seg)
+        # self.transciberModel.stream_segment_to(audio_path, self.handle_seg)
         
-        with open("segments.pkl", "rb") as f:
+        with open("outputs/segments_large.pkl", "rb") as f:
             self.segments = pickle.load(f)
 
         for seg in self.segments:
             self.handle_seg(seg)
 
-        pos_queries = [
-            # TASK SPECIFIC
-            "discussing insertion sort steps",
-            "explaining how insertion sort works",
-            "talking about array and inserting elements in order",
-            # META ACTIVITY
-            "students collaborating on a programming excersise",
-            "students solving an algorithm and data structures problem",
-            "students analyzing an array for sorting",
-            # ENVIRONMENT
-            "group work in a computer science class",
-            "students talking in a computer science class",
-            "discussion during algorithms and data structures class",
-            # VOCABULARY
-            "using terms like element, array, insert, sorted, next element",
-            "using numbers"
-        ]
-
-        neg_queries = [
-            "casual conversation",
-            "conversation unrelated to sorting algorithms",
-            "students joking or chatting socially",
-            "personal talk not related to algorithms"
-        ]
-
-        topic_phrases = [
-            "insertion sort",
-            "sorting an array",
-            "algorithm steps",
-            "insert element into correct position",
-            "sorted subarray",
-            "programming exercise",
-            "computer science assignment",
-            "array element comparison",
-            "shift elements to make space"
-            ]
-
-        keywords = ["element", "array", "insert", "sorted", "position",
-            "shift", "compare", "index", "next", "previous",
-            "iteration", "pass", "key", "number", "insertion sort"]
-
-        pos_embeds = self.semanticSearch.encode(pos_queries)
-        neg_embeds = self.semanticSearch.encode(neg_queries)
-        topic_vector = self.semanticSearch.topic_vec(topic_phrases)
         sentence_embeds = self.semanticSearch.encode(self.passage)
 
-        res = self.semanticSearch.similarity(pos_embeds, sentence_embeds)
+        res = self.semanticSearch.similarity(self.pos_embeds, sentence_embeds)
         pos_scores = self.max_score(res)
 
-        res = self.semanticSearch.similarity(neg_embeds, sentence_embeds)
+        res = self.semanticSearch.similarity(self.neg_embeds, sentence_embeds)
         neg_scores = self.max_score(res)
         
-        res = self.semanticSearch.similarity(topic_vector, sentence_embeds)
+        res = self.semanticSearch.similarity(self.topic_vector, sentence_embeds)
         topic_scores = self.max_score(res)
-        
-        keyword_count = [sum(1 for k in keywords if k in seg["text"].lower()) for seg in self.segments]
+
+        keyword_count = [sum(1 for k in self.keywords if k in seg["text"].lower()) for seg in self.segments]
         keyword_bonuses = [min(x * 0.05, 0.20) for x in keyword_count]
 
         final_scores = np.array(pos_scores) - np.array(neg_scores) + np.array(topic_scores) + np.array(keyword_bonuses)
 
-        with open("output/scores.txt", "w", encoding="utf-8") as f:
+        with open("outputs/scores.txt", "w", encoding="utf-8") as f:
             for i, seg in enumerate(self.segments):
                 print(f"[{final_scores[i]:.4f}] << {self.segments[i]['start']:.2f} : {self.segments[i]['end']:.2f} => {self.segments[i]['text']}", file=f)
                 print(f"{{ pos_score: {pos_scores[i]:.4f}, neg_score: {neg_scores[i]:.4f}, topic_score: {topic_scores[i]:.4f}, keyword_count: {keyword_count[i]} }}\n", file=f)
+
 
         # with open("output/segments.pkl", "wb") as f:
         #     pickle.dump(self.segments, f, protocol=pickle.HIGHEST_PROTOCOL)
--- analysis/main.py
+++ analysis/main.py
@@ -3,7 +3,48 @@ from contextProcessor import ContextProcessor
 def main():
     AUDIO_PATH = 'audio/pogil.wav'
 
-    p = ContextProcessor()
+    params = {
+        "pos_queries": [
+            # TASK SPECIFIC
+            "discussing insertion sort steps",
+            "explaining how insertion sort works",
+            "talking about array and inserting elements in order",
+            # META ACTIVITY
+            "students collaborating on a programming excersise",
+            "students solving an algorithm and data structures problem",
+            "students analyzing an array for sorting",
+            # ENVIRONMENT
+            "group work in a computer science class",
+            "students talking in a computer science class",
+            "discussion during algorithms and data structures class",
+            # VOCABULARY
+            "using terms like element, array, insert, sorted, next element",
+            "using numbers"
+        ],
+        "neg_queries": [
+            "casual conversation",
+            "joking or chatting socially",
+            "personal talk"
+        ],
+        "topic_phrases": [
+            "insertion sort",
+            "sorting an array",
+            "algorithm steps",
+            "insert element into correct position",
+            "sorted subarray",
+            "programming exercise",
+            "computer science assignment",
+            "array element comparison",
+            "shift elements to make space"
+        ],
+        "keywords": [
+            "element", "array", "insert", "sort", "position",
+            "shift", "compare", "index", "next", "previous",
+            "iteration", "pass", "key", "number", "loop", "value"
+        ]
+    }
+
+    p = ContextProcessor(params)
     
     p.process(AUDIO_PATH)
 
